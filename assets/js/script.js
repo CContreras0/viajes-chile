d30b49d9079e9fea676dc9a3b7141ad5af9bfaef
Claudia <cn.contrerasa@gmail.com>
2024-07-15 19:42:46 -0400

    Se agregaron tooltips con posicionamiento top a traves de js en formulario e iconos de la pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function(){
     console.log("ready!");
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+     // Se aplica posicionamiento top por defecto a todos los tooltips de la página
+    $('[data-toggle="tooltip"]').tooltip({
+        placement : 'top'
+    });
 });
